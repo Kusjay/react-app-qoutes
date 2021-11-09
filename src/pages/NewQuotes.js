74a60dbd4f5f3bc454a9ca312c431c2f940da7fd
Kelechi <kelechiuma@gmail.om>
2021-11-09 09:56:40 +0100

    Implementing programmatic imperative navigation

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,10 +1,16 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 import React from 'react';
 
 const NewQuotes = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
