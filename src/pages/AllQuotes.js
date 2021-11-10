6e8d2909b6147efbbe51c2f9c80de7c3ab0aeadb
Kelechi <kelechiuma@gmail.om>
2021-11-10 10:45:04 +0100

    Working with query params to sort list of quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -5,6 +5,8 @@ import QuoteList from '../components/quotes/QuoteList';
 const DUMMY_QUOTES = [
   { id: 'q1', author: 'Kusjay', text: 'My awesome quote' },
   { id: 'q2', author: 'Kelechi', text: 'My amazing quote' },
+  { id: 'q3', author: 'Kelechi', text: 'Another epic quote' },
+  { id: 'q4', author: 'Kelechi', text: 'Be nice quote' },
 ];
 
 const AllQuotes = () => {
